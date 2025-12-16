c964c825f76886ef88e8e9d22d82cb97679a4bc1
Pawel Bartusiak <21136755+uFloppyDisk@users.noreply.github.com>
2025-12-16 10:54:23 -0800

    fix(db-context): manager account seeding and Price column warn;

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,6 +15,39 @@ namespace EmailDemo.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSeeding((context, _) =>
+            {
+                var managerUser = new IdentityUser
+                {
+                    Id = "manager-user-id",
+                    UserName = "manager@home.com",
+                    NormalizedUserName = "MANAGER@HOME.COM",
+                    Email = "manager@home.com",
+                    NormalizedEmail = "MANAGER@HOME.COM",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString()
+                };
+
+                // Hash the password
+                var passwordHasher = new PasswordHasher<IdentityUser>();
+                managerUser.PasswordHash = passwordHasher.HashPassword(managerUser, "P@ssw0rd!");
+
+                var userRole = new IdentityUserRole<string>
+                {
+                    UserId = "manager-user-id",
+                    RoleId = "ad"
+                };
+
+                context.Add(managerUser);
+                context.Add(userRole);
+                context.SaveChanges();
+            });
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -28,36 +61,9 @@ namespace EmailDemo.Data
                 new IdentityRole { Id = "cu", Name = "Customer", NormalizedName = "CUSTOMER" }
             );
 
-            // =====================================================
-            // 2) Seed User (manager@home.com)
-            // =====================================================
-            var managerUser = new IdentityUser
-            {
-                Id = "manager-user-id",
-                UserName = "manager@home.com",
-                NormalizedUserName = "MANAGER@HOME.COM",
-                Email = "manager@home.com",
-                NormalizedEmail = "MANAGER@HOME.COM",
-                EmailConfirmed = true,
-                SecurityStamp = Guid.NewGuid().ToString()
-            };
-
-            // Hash the password
-            var passwordHasher = new PasswordHasher<IdentityUser>();
-            managerUser.PasswordHash = passwordHasher.HashPassword(managerUser, "P@ssw0rd!");
-
-            modelBuilder.Entity<IdentityUser>().HasData(managerUser);
-
-            // =====================================================
-            // 3) Assign User to Admin role
-            // =====================================================
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>
-                {
-                    UserId = "manager-user-id",
-                    RoleId = "ad"
-                }
-            );
+            // Fix Price datatype warn
+            modelBuilder.Entity<Product>()
+              .Property(obj => obj.Price).HasPrecision(10, 2);
         }
     }
 }
